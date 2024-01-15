64b78448566605f9071f7173bd97e7aa5a41b50d
Filipi565 <116827820+Filipi565@users.noreply.github.com>
2024-01-15 18:28:04 -0300

    Bug Fixes

--- src/pybash/_main.py
+++ src/pybash/_main.py
@@ -4,6 +4,14 @@ import sys
 from .constants import *
 from .functions import *
 from .util import args as _args
+import copy
+
+def import_ext(*args):
+    local = dict()
+    for ext in args:
+        exec(f"from {ext} import *", None, local)
+    globals().update(copy.deepcopy(local))
+    del local
 
 # This is to solve some bugs
 exit = lambda: None
@@ -13,19 +21,14 @@ def _run(command:str):
     __command = args[0].strip()
     args.remove(args[0])
     args = list(_args(args))
-
-    if __command == "import_ext":
-        local = dict()
-        for ext in args:
-            exec(f"from {ext} import *", None, local)
-        globals().update(local)
-        return
-    
-    if __command[0:2] in ("./", ".\\"):
-        __command = os.path.abspath(__command)
+    if __command.startswith((".\\", "./")):
+        __command = __command.replace(__command[0:2], os.path.abspath(__command) + os.sep)
         
     if __command in globals():
-        globals()[__command](*args)
+        try:
+            globals()[__command](*args)
+        except BaseException as e:
+            print(e)
         return
     try:
         sp.Popen([os.path.join(COMMANDS_PATH, __command), *args]).wait()
@@ -41,9 +44,9 @@ def _run(command:str):
 
 def main():
     try:
-        if sys.platform.startswith("win"):
-            os.system("title Bash")
         while True:
+            if sys.platform.startswith("win"):
+                os.system("title Bash")
             this_dir = os.getcwd()
             dir_for_print = this_dir.replace(USER_PATH, "~", 1)
             print(f"{GREEN}{USER_NAME}@{HOST_NAME} {WHITE}{BLUE}{dir_for_print}{WHITE}$", end=" ")
